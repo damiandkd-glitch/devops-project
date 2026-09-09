63158f7ddd62728f6f6fa950bccbb98e21106a70
Vincent <damian.dkd@gmail.com>
2026-09-09 12:18:01 +0200

    Aplikacja faktycznie odczytuje DB_PASSWORD ze zmiennej środowiskowej

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Cześć! Moja aplikacja działa w Kubernetes! Wersja 2 - GitOps działa i nie przestanie!'
+    db_password = os.environ.get('DB_PASSWORD', 'BRAK - zmienna nie znaleziona')
+    dlugosc_hasla = len(db_password)
+    return f'Cześć! Moja aplikacja działa w Kubernetes! Sekret DB_PASSWORD ma {dlugosc_hasla} znaków.'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
