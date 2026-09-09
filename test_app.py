7c5f89989061dd9dd29b6b527289eb638f30b6aa
Vincent <damian.dkd@gmail.com>
2026-09-09 20:02:30 +0200

    Naprawa testu - przywrócenie poprawnego sprawdzenia

--- test_app.py
+++ test_app.py
@@ -8,4 +8,4 @@ def test_home_page():
 def test_home_contains_text():
     client = app.test_client()
     response = client.get('/')
-    assert b'TEKST_KTOREGO_NIE_MA_NA_STRONIE' in response.data
+    assert b'Cze' in response.data
